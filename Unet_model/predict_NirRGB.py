e1bf1b44cc2737b9a00e0c67abf388c7515549b3
BaoGuang Li <rnlibaoguang@126.com>
2023-06-10 11:16:23 +0800

    Add files via upload

---
+++ Unet_model/predict_NirRGB.py
@@ -0,0 +1,128 @@
+
+import os
+import time
+import cv2
+import matplotlib.pyplot as plt
+
+import glob
+import tqdm
+import torch
+from torchvision import transforms
+import numpy as np
+from PIL import Image
+from src import UNet
+
+
+def time_synchronized():
+    torch.cuda.synchronize() if torch.cuda.is_available() else None
+    return time.time()
+
+
+def predict(img_path: str,weights_path: str):
+
+    classes = 1
+    channels = 4
+    device = 'cuda'
+    weights_path = weights_path
+    img_path = img_path
+    assert os.path.exists(weights_path), f"weights {weights_path} not found."
+    assert os.path.exists(img_path), f"image {img_path} not found."
+    
+    # get devices
+    device = torch.device(device if torch.cuda.is_available() else "cpu")
+    # print("using {} device.".format(device))
+
+    H, W = np.array(cv2.imread(img_path,-1)).shape[0], np.array(cv2.imread(img_path,-1)).shape[0]
+    # create model
+    model = UNet(in_channels=channels, num_classes=classes+1, base_c=64)
+
+    # load weights
+    model.load_state_dict(torch.load(weights_path, map_location='cpu')['model'])
+    model.to(device)
+
+    # from pil image to tensor and normalize
+    # data_transform = transforms.Compose([transforms.Resize(480),
+    #                                       transforms.ToTensor(),
+    #                                       transforms.Normalize(mean=(0.485, 0.456, 0.406),
+    #                                                           std=(0.229, 0.224, 0.225))])
+    data_transform = transforms.Compose([transforms.Resize(480),
+                                          transforms.ToTensor(),
+                                          transforms.Normalize(mean=(0.496, 0.370, 0.390, 0.362),
+                                                              std=(0.241, 0.229, 0.222, 0.219))])
+    
+    original_img = Image.open(img_path)
+    img = data_transform(original_img)
+    img = torch.unsqueeze(img, dim=0)
+
+    model.eval()  # 进入验证模式
+    with torch.no_grad():
+        # init model
+        img_height, img_width = img.shape[-2:]
+        init_img = torch.zeros((1, channels, img_height, img_width), device=device)
+        model(init_img)
+
+        t_start = time_synchronized()
+        output = model(img.to(device))
+        t_end = time_synchronized()
+        # print("inference+NMS time: {}".format(t_end - t_start))
+
+        prediction = output['out'].argmax(1).squeeze(0)
+        prediction = prediction.to("cpu").numpy().astype(np.uint8)
+        mask = cv2.resize(prediction, (H, W), interpolation=cv2.INTER_NEAREST)
+
+        return mask
+    
+
+if __name__ == '__main__':
+    print('\n-------------Processing Start!!!-------------\n')
+    
+    # weights_path = r"E:\GID\exp\train_val\save_weights_old\unet_best_model_13.pth"
+    weights_path = r"E:\GID_experiment\allocate_data_492\train_unet\save_weights_unet\unet_best_model_epoch6_dice0.8191751837730408.pth"
+    imgs_path = r"E:\GID_experiment\allocate_data_492\test\image_NirRGB"
+    masks_path = os.path.dirname(imgs_path) + "\\test_result_unet_epoch6"
+    if not os.path.exists(masks_path):
+        os.makedirs(masks_path)
+        
+    imgs_name_list = glob.glob(imgs_path + '\\*.tif')
+    
+    start_time = time.time()
+    for img in tqdm.tqdm(imgs_name_list, desc='Precessing'):
+        mask = predict(img,weights_path)
+        img_name = os.path.basename(img)
+        cv2.imwrite(os.path.join(masks_path,img_name.split('.')[0]+".tif"), mask*255)
+        
+    print('\n\n-------------Congratulations! Processing Done!!!-------------')
+    total_time = time.time() - start_time
+    print("Total training time : {:.0f}h {:.0f}m {:.2f}s\n".format(
+        total_time//3600, (total_time%3600)//60, (total_time%3600)%60))
+
+# if __name__ == '__main__':
+    
+#     weights_path = r"E:\gaofen-competition\GaoFen_challenge_TVT\train_val\save_weights\Unet_best_model.pth"
+#     path = r"E:\gaofen-competition\GaoFen_challenge_TVT\test"
+#     # path = r"E:\gaofen-competition\GaoFen_challenge_TVT\train_val"
+    
+#     ids = 540
+#     example_image = os.path.join(path,'Images\\' + str(ids) + "_img.jpg")
+#     example_mask = os.path.join(path,'Class\\' + str(ids) + "_img.png")
+ 
+#     mask = predict(example_image,weights_path)
+#     image = cv2.cvtColor(cv2.imread(example_image), cv2.COLOR_BGR2RGB)
+#     refer_mask = cv2.cvtColor(cv2.imread(example_mask),cv2.COLOR_BGR2GRAY)
+
+#     #plot the image, mask and multiplied together
+#     fig, (ax1, ax2, ax3) = plt.subplots(1,3, figsize=(20, 15))
+
+#     ax1.imshow(image)
+#     ax1.axis('off')
+#     ax1.set_title('Raw image',fontsize=26)
+
+#     ax2.imshow(mask)
+#     ax2.axis('off')
+#     ax2.set_title('U-net',fontsize=26)
+
+#     # ax3.imshow(refer_mask,cmap='gray')
+#     ax3.imshow(refer_mask)
+#     ax3.axis('off')
+#     ax3.set_title('Ground truth',fontsize=26)
+
---
+++ Unet_model/train.py
@@ -0,0 +1,163 @@
+import os
+import time
+import datetime
+import torch
+from src import UNet
+from train_utils import train_one_epoch, evaluate, create_lr_scheduler
+from MyDataset_v2 import my_dataset, read_split_data
+import albumentations as A
+
+
+def main(args):
+    name = 'unet'
+    batch_size = args.batch_size
+    model_path = args.model_path
+    if not os.path.exists(model_path):
+        os.makedirs(model_path)
+    
+    device = torch.device(args.device if torch.cuda.is_available() else "cpu")
+    # segmentation nun_classes + background
+    num_classes = args.num_classes + 1
+
+    # 用来保存训练以及验证过程中信息
+    results_file = model_path + "\\" + name + "_{}.txt".format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+    # transforms
+    size = 480
+    train_img_aug = A.Compose([
+        A.Resize(width=size, height=size),
+        A.RandomCrop(width=size, height=size),
+        A.HorizontalFlip(p=0.5),
+        A.VerticalFlip(p=0.5),
+        A.Flip(p=0.5),
+        A.RandomGridShuffle(grid=(2, 2), p=0.5),
+        A.Rotate(limit=180, p=0.5),
+    ])
+    val_img_aug = A.Compose([A.Resize(width=size, height=size),])
+
+    train_images_path, train_masks_path, val_images_path, val_masks_path = read_split_data(
+        args.data_path,val_rate=0.25, images_format='.tif',masks_format='.tif')
+
+    train_dataset = my_dataset(images_path=train_images_path,
+                               masks_path=train_masks_path,
+                               transforms=train_img_aug)
+    val_dataset = my_dataset(images_path=val_images_path,
+                               masks_path=val_masks_path,
+                               transforms=None)  
+    
+    train_loader = torch.utils.data.DataLoader(train_dataset,
+                                               batch_size=batch_size,
+                                               num_workers=0,
+                                               shuffle=True,
+                                               drop_last=True,
+                                               pin_memory=True)
+
+    val_loader = torch.utils.data.DataLoader(val_dataset,
+                                             batch_size=1,
+                                             num_workers=0,
+                                             drop_last=False,
+                                             pin_memory=True)
+    # model 需要修改
+    model = UNet(in_channels=args.in_ch, num_classes=num_classes, base_c=64)
+    model.to(device)
+    
+    # optimizer 需要修改
+    params_to_optimize = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(
+        params_to_optimize,
+        lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay
+    )
+
+    scaler = torch.cuda.amp.GradScaler() if args.amp else None
+    # 创建学习率更新策略，这里是每个step更新一次(不是每个epoch)
+    lr_scheduler = create_lr_scheduler(optimizer, len(train_loader), args.epochs, warmup=True)
+
+    best_dice = 0.
+    start_time = time.time()
+    for epoch in range(0, args.epochs):
+        start_time_epoch = time.time()
+        mean_loss, lr = train_one_epoch(model, optimizer, train_loader, device, epoch, num_classes,
+                                        lr_scheduler=lr_scheduler, print_freq=args.print_freq, scaler=scaler)
+        
+        confmat, dice = evaluate(model, val_loader, device=device, num_classes=num_classes)
+        val_info = str(confmat)
+        print(val_info)
+        print(f"dice coefficient: {dice*100:.3f}")
+        # write into txt
+        with open(results_file, "a") as f:
+            # 记录每个epoch对应的train_loss、lr以及验证集各指标
+            train_info = f"[epoch: {epoch+1}]\n" \
+                         f"train_loss: {mean_loss:.4f}\n" \
+                         f"lr: {lr:.6f}\n" \
+                         f"dice coefficient: {dice*100:.3f}\n"
+            f.write(train_info + val_info + "\n\n")
+
+        if args.save_best is True:
+            if best_dice < dice:
+                best_dice = dice
+            else:
+                continue
+
+        save_file = {"model": model.state_dict(),
+                     "optimizer": optimizer.state_dict(),
+                     "lr_scheduler": lr_scheduler.state_dict(),
+                     "epoch": epoch,
+                     "args": args}
+        if args.amp:
+            save_file["scaler"] = scaler.state_dict()
+        
+        if args.save_best is True:
+            torch.save(save_file, model_path + "\\"+name+"_best_model_epoch{}_dice{}.pth".format(epoch+1,dice))
+        elif epoch % 20 == 0:
+            torch.save(save_file, model_path + "\\"+name+"_model_epoch{}_dice{}.pth".format(epoch+1,dice))
+        elif epoch == args.epochs - 1:
+            torch.save(save_file, model_path + "\\"+name+"_model_epoch{}_dice{}.pth".format(epoch+1,dice))
+
+        epoch_time = time.time() - start_time_epoch
+        print("Epoch {}/{} : {:.0f}m {:.2f}s\n".format(epoch+1, args.epochs, epoch_time // 60, epoch_time % 60))
+
+    total_time = time.time() - start_time
+    print("Total training time : {:.0f}h {:.0f}m {:.2f}s\n".format(
+        total_time//3600, (total_time%3600)//60, (total_time%3600)%60))
+
+def parse_args():
+    import argparse
+    parser = argparse.ArgumentParser(description="pytorch model training")
+
+    data_path = r"E:\GID\exp\train_val"
+    parser.add_argument("--data-path", default=data_path, help="Dataset root")
+    model_dir = r"./save_weights_unet"
+    parser.add_argument("--model-path", default=model_dir, help="Saved model root")
+    # exclude background
+    parser.add_argument("--num-classes", default=1, type=int)
+    parser.add_argument("--aux", default=True, type=bool, help="auxilier loss")
+    parser.add_argument("--device", default="cuda", help="training device")
+    parser.add_argument("-b", "--batch-size", default=3, type=int)
+    parser.add_argument("--epochs", default=180, type=int, metavar="N",
+                        help="number of total epochs to train")
+
+    parser.add_argument('--lr', default=0.001, type=float, help='initial learning rate')
+    parser.add_argument('--momentum', default=0.9, type=float, metavar='M',
+                        help='momentum')
+    parser.add_argument('--wd', '--weight-decay', default=1e-4, type=float,
+                        metavar='W', help='weight decay (default: 1e-4)',
+                        dest='weight_decay')
+    parser.add_argument('--print-freq', default=50, type=int, help='print frequency')
+
+    parser.add_argument('--save-best', default=True, type=bool, help='only save best dice weights')
+    # Mixed precision training parameters
+    parser.add_argument("--amp", default=False, type=bool,help="Use torch.cuda.amp for mixed precision training")
+    args = parser.parse_args()
+
+    return args
+
+if __name__ == '__main__':
+
+    args = parse_args()
+
+    args.data_path = r"E:\GID_experiment\allocate_data_492\train_unet"
+    args.model_path = r"E:\GID_experiment\allocate_data_492\train_unet\save_weights_unet"    
+    args.in_ch = 4
+    main(args)
+
+
